34f43741177d29800947b53373ec4469faf29a8f
Formasitchijoh <76694292+Formasitchijoh@users.noreply.github.com>
2022-05-19 00:06:31 +0000

    Accessing the element of a vector

---
+++ C++_Work/Accesing_Element.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+	vector<int> num {1,2,3,4,5};
+
+	cout << "Element at index 0:" <<num[1] << endl;
+	cout <<"Element at index 2 : " <<num[3] <<endl;
+	cout <<"Elemet at index 4 : " << num[4] <<endl;
+
+	return 0;
+}
