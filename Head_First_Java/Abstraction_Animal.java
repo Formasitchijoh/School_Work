b736e4cdd3a0671e8e926c8ce886b77ad510eb11
Formasitchijoh <76694292+Formasitchijoh@users.noreply.github.com>
2022-07-06 22:45:41 +0000

    Abstraction in java

---
+++ Head_First_Java/Abstraction_Animal.java
@@ -0,0 +1,18 @@
+package W3School;
+
+
+//abstract class
+public abstract class Abstraction_Animal {
+
+	
+	//abstract method does not have a body
+	
+	public abstract void animalSound();
+	
+	//regular method
+	
+	public void sleep() {
+		System.out.println("Zzz");
+	}
+}
+
---
+++ Head_First_Java/Animal.java
@@ -0,0 +1,8 @@
+package W3School;
+
+//An interface this is a completely abstract class
+public interface Animal {
+
+	public void animalSound(); // interface method does not have a body
+	public void sleep(); //interface method does not have a body
+}
