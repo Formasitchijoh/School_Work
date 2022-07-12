244790be2e0d4cf01d8d53fc2bf0ecbeb806b3a0
Formasitchijoh <76694292+Formasitchijoh@users.noreply.github.com>
2022-07-12 21:00:24 +0000

    runnbale for creating threads

---
+++ Head_First_Java/MyRunnable.java
@@ -0,0 +1,26 @@
+package Threads;
+
+public class MyRunnable implements Runnable{
+
+	public void run() {
+		go();
+	}
+	
+	public void go() {
+		
+		try {
+			Thread.sleep(2000);
+			
+		}
+		
+		catch(InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		
+		doMore();
+	}
+	
+	public void doMore() {
+		System.out.println("top of the stack");
+	}
+}
